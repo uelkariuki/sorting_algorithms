5e3f5bafad05767200049557d6ec823c99408bda
uelkariuki <cratzuel@gmail.com>
2023-06-25 21:03:50 +0000

    Task 3. Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+#include "swap.c"
+
+/**
+ * partition- function to find the correct index of the pivot
+ * @array: the array to be sorted
+ * @low: the firs index
+ * @high: the last index
+ * Return: the correct index of the pivot
+ */
+
+int partition(int *array, int low , int high)
+{
+	int pivot, i, j;
+
+	pivot = array[high];
+	i = low - 1;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[j], &array[i]);
+
+		}
+
+	}
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+/**
+ * quick- recursive helper function to sort the array
+ * @array: array to be sorted
+ * @low: first index
+ * @high: last index
+ */
+
+void quick(int *array, int low, int high)
+{
+	int pivot_idx;
+
+	if (low < high)
+	{
+		pivot_idx = partition(array, low, high);
+		/*print_array(arraiy, high + 1);*/
+
+		quick(array, low, pivot_idx - 1);
+		quick(array, pivot_idx + 1, high);
+
+	}
+}
+
+/**
+ * quick_sort- function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	int n;
+
+	n = size;
+
+	if (n < 2)
+		return;
+
+	quick(array, 0, size - 1);
+}
--- sort.h
+++ sort.h
@@ -21,4 +21,5 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
+
 #endif /*SORT_H*/
