722b84cb2c100c27697c682a4c0cdde435588509
uelkariuki <cratzuel@gmail.com>
2023-06-26 04:28:49 +0000

    Task 3. Quick sort fix 4

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -65,8 +65,8 @@ void quick(int *array, int low, int high, size_t size)
 void quick_sort(int *array, size_t size)
 {
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	quick(array, 0, size -1 , size);
+	quick(array, 0, size - 1, size);
 }
