7e68e47513881188b72f3c98595da840094046b4
uelkariuki <cratzuel@gmail.com>
2023-06-27 23:40:53 +0000

    Task 11.2 Quick Sort - Hoare Partition scheme

---
+++ 1000-sort_deck.c
@@ -0,0 +1,15 @@
+#include "deck.h"
+
+/**
+ * sort_deck-  function that sorts a deck of cards
+ * @deck: the deck of cards to be sorted
+ *
+ */
+
+void sort_deck(deck_node_t **deck)
+{
+
+
+
+
+}
---
+++ 106-bitonic_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * bitonic_sort- a function that sorts an array of integers in
+ * ascending order using the Bitonic sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
+ */
+
+void bitonic_sort(int *array, size_t size)
+{
+
+
+
+}
